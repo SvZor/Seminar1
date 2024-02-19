f5c52ec3cc861b2a50d5d7d4fcb785e3c4cc0bd4
SvZor <zorinasp@yandex.ru>
2024-02-19 22:04:41 +0900

    семинар

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Это строка выводит текст 
+// Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая на вход принимает
+// число и выдает его квадрат (число умноженное
+// на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+//Console.WriteLine("Введите число");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//int result = num * num;
+//Console.WriteLine(result);
