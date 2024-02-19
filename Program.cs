3e241ef96b3dfdb8ec94c6f3f88a220b029626c2
SvZor <zorinasp@yandex.ru>
2024-02-19 23:15:51 +0900

    семинар++задача7.1

--- Program.cs
+++ Program.cs
@@ -106,8 +106,10 @@
 // 782 -> 2
 // 918 -> 8
 
-Console.WriteLine("Введите число");
-int num=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32(Console.ReadLine());
 
-int result = num % 10;
-Console.WriteLine(result);
+// int result = num % 10;
+// Console.WriteLine(result);
+
+
